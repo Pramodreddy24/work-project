64864fc76eb6b2e06d7e82d1f5d8c7f7951532cf
pramodh <pramodreddyv8@gmail.com>
2025-02-10 12:25:56 -0600

    my new repo

---
+++ SimpleIf.java
@@ -0,0 +1,9 @@
+public class SimpleIf {
+    public static void main(String[] args) {
+        int number = 5;
+
+        if (number == 5) {
+            System.out.println("Number is 5");
+        }
+    }
+}
